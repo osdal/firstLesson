5a86df75eb6c80c59385f609807a3fbe81789b51
osdal <osdal67@gmail.com>
2018-08-08 14:59:40 +0300

    Первое задание

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+﻿summMonth = prompt('Ваш бюджет на месяц?');
+nameShop = prompt('Название вашего магазина?');
+mainList = {
+	summMonth: 1000,
+	nameShop: "Веллнес",
+	shopGoods: ['Aplle', 'Milk'],
+	employers: {
+		director: 'Adam Smith',
+		worker: 'John Walker',
+		keeper: 'Jack Sparrow'
+	},
+	open: true
+}
+
+var shopGoods = [prompt('Какой тип товаров будем продавать?'),
+				 prompt('Какой тип товаров будем продавать?'),
+				 prompt('Какой тип товаров будем продавать?')];
+
+// Бюджет за 1 день
+alert('Бюджет за 1 день = '+ summMonth/30);

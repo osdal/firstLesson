31f840a4943c5530ed699fe9c3569a01ebab5b97
osdal <osdal67@gmail.com>
2018-08-09 19:14:52 +0300

    Второе задание

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,54 @@
-﻿var summMonth = prompt('Ваш бюджет на месяц?'),
+﻿let summMonth = prompt('Ваш бюджет на месяц?'),
 	nameShop = prompt('Название вашего магазина?'),
-	shopGoods = [prompt('Какой тип товаров будем продавать?'),
-				 	 prompt('Какой тип товаров будем продавать?'),
-				 	 prompt('Какой тип товаров будем продавать?')];
-	
-
-var mainList = {
+	mainList = {
 	summMonth: summMonth,
 	nameShop: nameShop,
-	shopGoods: shopGoods,
+	shopGoods: [],
 	employers: {
 		director: 'Adam Smith',
 		worker: 'John Walker',
 		keeper: 'Jack Sparrow'
 	},
-	open: true
+	open: true,
+	},
+	time = 32;
+
+for(let i = 0; i < 5; i++) {
+
+	let a = prompt('Какой тип товаров будем продавать?');
+	
+	if ((typeof(a))  === 'string' && (typeof(a))  != null && a != '' && a.length < 50) {
+		console.log('Все верно!');
+		mainList.shopGoods[i] = a;
+	} else {
+		console.log('Вы ввели неправильное название товара');
+		i--;
+	}
 }
+
+/*for(let i = 5; i > 0; i--) {
+
+	let a = prompt('Какой тип товаров будем продавать?');
+	
+	if ((typeof(a))  === 'string' && (typeof(a))  != null && a != '' && a.length < 50) {
+		console.log('Все верно!');
+		mainList.shopGoods[i] = a;
+	} else {
+		console.log('Вы ввели неправильное название товара');
+		i++;
+	}
+}*/
+
+if (time < 0) {
+	console.log('Такого не может быть');
+} else if (time > 0 && time < 20) {
+		console.log('Время работать');
+	} else if (time < 24) {
+		console.log('Уже поздно');
+	} else {
+		console.log('В сутках только 24 часа');
+	}
+
 
 // Бюджет за 1 день
 alert('Бюджет за 1 день = '+ mainList.summMonth/30);
